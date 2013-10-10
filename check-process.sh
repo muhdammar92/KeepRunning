6e8c7763424633d0f02f34dbea8108bf5f4cb0b8
Muhamad Ammar <muhd.ammar92@gmail.com>
2013-10-10 16:37:47 +0800

    Update check-process.sh
    add do while .. repeat process automatically

--- check-process.sh
+++ check-process.sh
@@ -1,13 +1,14 @@
+
 #!/bin/bash
 
-counter = 0
+counter=0
 
 echo "---------------------------"
 echo "Check all important process"
 echo "---------------------------"
 
 
-while [ $counter eq 0 ]; do
+while [ $counter == 0 ]; do
 # Check if honeymap is running
 
 if ps aux | grep "[s]erver" > /dev/null
@@ -15,7 +16,7 @@ then
   echo "Honeymap is Running in background mode"
   echo ""
 else
-      echo "Honeymap is not running at " date > /var/log/keep_running.log
+      echo "Honeymap is not running at " $(date) > /var/log/keep_running.log
       /var/www/honeymap/server/server > /dev/null 2>&1 &
 fi
 
@@ -24,12 +25,10 @@ if ps aux | grep "[g]eoloc.py" > /dev/null
 then
   echo "Geoloc.py is Running in background mode"
 else
-    echo "Geoloc is not running at " date > /var/log/keep_running.log
+    echo "Geoloc is not running at " $(date) > /var/log/keep_running.log
     
     python  /opt/hpfeeds/examples/geoloc/geoloc.py > /dev/null 2>&1 &
 fi
 
 done
-
-
 
